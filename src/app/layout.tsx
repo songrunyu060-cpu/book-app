f1a2e65b4e990ef94f8d0516d13abe84f59b4b8d
songrunyu <songrunyu060@gmail.com>
2026-05-19 00:18:26 +0800

    兼容

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,8 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono, Inter } from "next/font/google"
 import Script from "next/script"
 import "./globals.css"
 import { AuthSessionProvider } from "@/components/providers/session-provider"
 import { cn } from "@/lib/utils"
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
 
 /**
  * 用于设置页面标题和描述
@@ -37,14 +21,7 @@ export default async function RootLayout({
   return (
     <html
       lang="zh-CN"
-      className={cn(
-        "h-full",
-        "antialiased",
-        geistSans.variable,
-        geistMono.variable,
-        "font-sans",
-        inter.variable
-      )}
+      className={cn("h-full", "antialiased", "font-sans")}
       suppressHydrationWarning
     >
       <head>
